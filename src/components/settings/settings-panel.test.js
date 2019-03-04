5fb0f9c4bb59a222a32d24f83fce29c3a8af2918
harunhasdal <harun.hasdal@rma-consulting.com>
2019-03-04 21:47:32 +0000

    Remove unused files

--- src/components/settings/settings-panel.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import SettingsPanel from "./settings-panel";
-
-it("renders html form", () => {
-  const wrapper = shallow(<SettingsPanel />);
-  expect(wrapper.find("form").exists()).toBe(true);
-});
